8d1f32de681982e31165c116fc0800e31185c0e8
Coracias30 <asalido@eeza.csic.es>
2022-09-19 17:11:44 +0200

    Añado nueva figura

--- prueba.R
+++ prueba.R
@@ -2,5 +2,19 @@ library(tidyverse)
 datos <- read_csv("github_data.csv")
 
 ggplot(datos)+
-  geom_point(aes(work.hours, happiness))
+  geom_point(aes(work.hours, happiness))+
+  
 #hola guapa!  
+  
+  
+#nueva grafica
+
+datos %>%
+  ggplot(aes(x=work.hours, y=happiness))+
+  geom_line(color="blue")
+  geom_point(shape=20, color= "black", fill=#69b3a2, size=6) +
+  theme_ipsum() +
+    ggtitle ("prueba graf")
+               
+
+
